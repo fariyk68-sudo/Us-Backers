dea71c4fa508844e34fd68a985b12de6b823308b
fariyk68-sudo <fariyk68@gmail.com>
2026-02-07 22:45:38 +0500

    Add this perfect

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,21 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 3000,
+    host: true
   },
   build: {
     outDir: 'dist',
-  }
+    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
 });
